3e6ec58a7056801c21f4002c20c78367414fe3ad
PianoFF <Fangfei1star@gmail.com>
2019-11-27 12:05:03 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,14 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
+  #custom validation
   validate :is_title_case 
+  #lifecycle method:
+  # This gets called after validation: before_save :make_title_case 
+  #If performing actions, use before_save
+  #modifying an arribute of the model, use before_validation
+  before_validation :make_title_case
+  before_save :email_author_about_post
 
   private
 
@@ -12,6 +19,11 @@ class Post < ActiveRecord::Base
   end
 
   def make_title_case
+    #rails provides a string#titlecase method 
     self.title = self.title.titlecase
   end
+
+  def email_author_about_post
+
+  end 
 end
